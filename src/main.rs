9e475ab8de6fc2337fd722ef03ef707446886075
Ashish Kumer Shill <ashish@brillmark.com>
2024-04-03 02:26:05 +0600

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
